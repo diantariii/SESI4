8c6333cb8e75b508178e7f42182572f8d517b04b
kadekdiantarii_2201010330 <119943301+diantariii@users.noreply.github.com>
2023-04-26 19:30:27 +0800

    Add files via upload

---
+++ SESI4/latihan01.php
@@ -0,0 +1,51 @@
+<?php
+    $usr= "";
+    $pwd= "";
+    $salah = 0;
+    if(isset ($_GET["txUSER"])){
+        $usr=$_GET["txUSER"];
+        $pwd=$_GET["txPASS"];
+
+        if($usr==""){
+            $salah= 1;
+        }else{
+            echo"Isi dari Field User: ".$usr;
+        }
+        if($pwd==""){
+            $salah= 1;
+        }else{
+            echo"Isi dari Field Password: ".$pwd;
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Dengan Medote GET</title>
+</head>
+<body>
+<?php
+    if($salah==1){
+        echo"<div style='color: red'> User Name/Password masih kosong</div>";
+    }
+?>
+    <form method="GET" action="latihan01.php">
+        <div>
+            User Name
+            <input type="text" name="txUSER" required>
+        </div>
+        <div>
+            Password
+            <input type="password" name="txPASS" required>
+        </div>
+        <div>
+            <button type="submit"> login </button>
+        </div>
+    
+    </form>
+    
+</body>
+</html>
